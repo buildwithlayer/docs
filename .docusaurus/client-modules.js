439a97ac807cd079c129630a35103b2d64b605a2
opticayaan <ayaan.gill@me.com>
2025-01-08 10:43:22 -0500

    Added blog post

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/ayaangill/Desktop/Layer/Repos/docs/src/css/custom.css"),
+];
