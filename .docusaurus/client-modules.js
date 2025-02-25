cef8d83c0b01ffa01b3496b74bd8b63a74f13712
opticayaan <ayaan.gill@me.com>
2025-02-25 12:05:29 -0500

    Updated image for AI GTM Blog

--- .docusaurus/client-modules.js
+++ .docusaurus/client-modules.js
@@ -1,5 +1,4 @@
 export default [
-  require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/@docusaurus/plugin-google-gtag/lib/gtag"),
   require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/infima/dist/css/default/default.css"),
   require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
   require("/Users/ayaangill/Desktop/Layer/Repos/docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
